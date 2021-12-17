4f14807fae21100910f6adc46afd1c0fc4544945
David Castrillón <david.castri@hotmail.es>
2021-12-17 17:47:23 +0100

    Muestro y oculto pop-up

--- js/pop-up.script.js
+++ js/pop-up.script.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
-    console.log("Hola Mundo");
+
+    //Evento click en botón submit
+    $('#submit-button').click(function(event) {
+
+        //Evito comportamiento por defecto
+        event.preventDefault();
+
+        //Muestro pop-up
+        $('.popup').show();
+        
+    });
+
+    //Evento click en botón X de pop-up
+    $('.caption i').click(function() {
+
+        //Oculto pop-up
+        $('.popup').hide();
+
+    });
+
+    //Si se apreta fuera de .caption, se cierra pop-up
+    $('.popup').click(function(event) {
+
+        //Guardamos el elemento clickado
+        let elementClicked = event.target;
+
+        //Si el elemento clickado no está dentro del recuadro del popup, cerramos el popup
+        if (!$.contains(document.getElementById('pop-up'), elementClicked)) {
+            $('.popup').hide();
+        }
+
+    }); 
+    
 });
